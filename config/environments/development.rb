a235d0b9be38ac072f922495441be0380564a4ec
Daniil Sivakov <daniil.sivakov@wse-144-71b.icx.local>
2021-09-23 19:00:51 +0300

    Added email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: "utf-8"
 end
