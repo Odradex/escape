fa16a73b882410d1e3611ee46a75f5828413bd9b
Daniil Sivakov <sivakov.daniil@gmail.com>
2021-09-29 14:55:02 +0300

    add default avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,24 +14,22 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*args)
+    # For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_url("default-avatar.png")
+    "default-avatar.png"
+  end
 
   # Process files as they are uploaded:
   process scale: [200, 200]
-  
   def scale(width, height)
     resize_to_fill(width, height)
   end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
