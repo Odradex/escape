61b64672b23987e5bc5afd5e13d138aaafed9072
Daniil Sivakov <sivakov.daniil@gmail.com>
2021-10-06 14:48:05 +0300

    styling fixes

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,33 +1,24 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: %i[show edit update destroy]
 
-  # GET /organizations or /organizations.json
   def index
     @organizations = if current_user.admin?
                        Organization.all
                      else
-                       Organization.select { |org| org.user == current_user }
+                       Organization.where(user: current_user)
                      end
   end
 
-  # GET /organizations/1 or /organizations/1.json
   def show; end
 
-  # GET /organizations/new
   def new
     @organization = Organization.new
   end
 
-  # GET /organizations/1/edit
-  def edit
-    @editing = true
-  end
+  def edit; end
 
-  # POST /organizations or /organizations.json
   def create
-    @editing = false
     @organization = User.find(organization_params[:user_id]).organizations.new(organization_params)
-    config.logger.debug organization_params
     if @organization.save
       redirect_to @organization, notice: 'Organization was successfully created.'
     else
@@ -35,7 +26,6 @@ class OrganizationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /organizations/1 or /organizations/1.json
   def update
     if @organization.update(organization_params)
       redirect_to @organization, notice: 'Organization was successfully updated.'
@@ -44,7 +34,6 @@ class OrganizationsController < ApplicationController
     end
   end
 
-  # DELETE /organizations/1 or /organizations/1.json
   def destroy
     @organization.destroy
     redirect_to organizations_url, notice: 'Organization was successfully destroyed.'
@@ -52,12 +41,10 @@ class OrganizationsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_organization
     @organization = Organization.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def organization_params
     params.require(:organization).permit(:name, :user_id, :adress, :opening_time, :closing_time)
   end
--- db/migrate/20211001152003_rename_owner_column_in_organizations.rb
+++ db/migrate/20211001152003_rename_owner_column_in_organizations.rb
@@ -1,6 +1,5 @@
 class RenameOwnerColumnInOrganizations < ActiveRecord::Migration[6.1]
   def change
     rename_column :organizations, :owner_id, :user_id
-    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
   end
 end
